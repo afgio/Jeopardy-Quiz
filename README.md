d15aa97490a3d1e7e8ee3cd54ee8ea71f0e4c751
Antonio <antonio.g.93@hotmail.com>
2023-01-20 17:53:49 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# Jeopardy!-Quiz
+
+This app uses Axios to make API calls to the jservice API (http://jservice.io/).  When users click the "New Question" button, a GET request is made via an Axios call to the API which returns a JSON object containing a question and other relevant data such as the correct answer as well as the question's point value. This data is then displayed to the player to simulate a Jeopardy! quiz.   
---
+++ app.js
@@ -0,0 +1,57 @@
+const questionText = document.querySelector('#question-text');
+const submit = document.querySelector('#submit');
+const newQuestion = document.querySelector('#new-question-button');
+const category = document.querySelector('#category-text');
+const value = document.querySelector('#value-text');
+const points_text = document.querySelector('#points-text');
+const form = document.querySelector('#answer');
+const answerInput = document.querySelector('#answer-input')
+const result = document.querySelector('#result');
+const give_up = document.querySelector('#giveup');
+
+let answer = "";
+let input = "";
+let points = 0;
+
+const newJeopardyQuestion = async () => {
+    try {
+        const res = await axios.get('http://jservice.io/api/random');
+        submit.disabled = false;
+        questionText.innerHTML = res.data[0].question;
+        category.innerHTML = res.data[0].category.title;
+        value.innerHTML = res.data[0].value;
+        answer = res.data[0].answer.toLowerCase().replace(/<(.|\n)*?>/g, '');
+        console.log(res.data)
+        console.log(answer);
+    } catch (error) {
+        console.log("Error: ", error)
+    }
+}
+
+newJeopardyQuestion();
+
+newQuestion.addEventListener('click', () => {
+    newJeopardyQuestion();
+    result.innerHTML = "";
+    answerInput.value = "";
+    give_up.disabled = false;
+})
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (answer === answerInput.value.toLowerCase()) {
+        submit.disabled = true;
+        give_up.disabled = true;
+        result.innerHTML = 'Correct! ✅';
+        points += parseInt(value.innerHTML);
+        points_text.innerHTML = points.toString();
+    } else {
+        result.innerHTML = 'Incorrect ❌'
+    }
+})
+
+give_up.addEventListener('click', () => {
+    result.innerHTML = `What is ${answer}`;
+    submit.disabled = true;
+})
+
